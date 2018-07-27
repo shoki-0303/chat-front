1d82bde85092e17fb2a047d71a24208f0c562727
shoki-0303 <shoki522.33333@gmail.com>
2018-07-27 20:27:13 +0900

    グループ編集＆更新機能の修正
    グループ編集、更新機能の修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,6 +12,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -24,6 +25,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
+      @group.users << current_user
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
